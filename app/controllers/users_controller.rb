ccb4637ca61d62fe9fcc2976323917e84518bd4e
theoblivious <hashimoto.kenn@gmail.com>
2014-10-21 19:21:38 -0400

    made some headyway

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  def index
+    @users=Users.all
+  end
+
+
+  def new
+    @users=User.new
+  end
+
+
+  def create
+    @user= User.new(user_params)  #Remember User.new is creating a new isntance of the database object but not saving
+    if @user.save                  #if it passes all of the parameters, its going to render the user and save else its going back to .new
+      redirect_to @user            #save  is a helper method. save is goign to try and save base ond validations
+    else
+      render :new
+    end
+  end
+
+
+  def update
+
+  end
+
+
+
+  def show
+
+  end
+
+
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
