6902f0c0ad1b7c59747a0f223781148f84a1cd5a
theoblivious <hashimoto.kenn@gmail.com>
2014-10-21 21:32:28 -0400

    CRUD application works for all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
